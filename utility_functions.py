45a0bae431bc53b033bdaed7956a6036845ae32b
deeplearner0731 <33180986+deeplearner0731@users.noreply.github.com>
2020-10-28 18:10:52 -0400

    10/28

--- utility_functions.py
+++ utility_functions.py
@@ -8,51 +8,30 @@ from matplotlib import pyplot as plt
 
 
 
-def train_accuracy(theta_var,vr_beta_mean,vr_beta_rho,vr_gamma_mean,vr_gamma_rho):
+def theta_(x,beta,gamma):
+    n=x.shape[0]
+    x=np.column_stack((np.ones(n),x))
+    val1=np.tensordot(gamma,x,(1,1))
+    val2=1.0/(1+np.exp(-val1))
+    theta=beta[0]+np.dot(beta[1::].reshape(1,kn),val2)
+    return theta
+
+def train_accuracy(False,vr_beta_mean,vr_beta_rho,vr_gamma_mean,vr_gamma_rho):
     Beta,Gamma=q_simulate(10000,vr_beta_mean,vr_beta_rho,vr_gamma_mean,vr_gamma_rho)
-    beta=np.apply_along_axis(np.mean, 1, Beta)
-    gamma=np.apply_along_axis(np.mean, 2, Gamma)
-    n=X.shape[0]
-    X1=np.column_stack((np.ones(n),X))
-    V1=np.dot(gamma,X1.T)
-    V2=1.0/(1+np.exp(-V1))
-    theta=beta[0]+np.dot(beta[1::].reshape(1,kn),V2)
-    if theta_var==True:
-        var_theta=np.std(theta,axis=1)
-    
-        Y_est=np.array([0 if x<0.5 else 1 for x in 1.0/(1+np.exp(-theta.T))])
-        Y_est=Y_est.reshape((len(Y_est),))
-        acc=sum(Y==Y_est)*1.0/len(Y)
-        return acc,var_theta
-    else:
-        Y_est=np.array([0 if x<0.5 else 1 for x in 1.0/(1+np.exp(-theta.T))])
-        Y_est=Y_est.reshape((len(Y_est),))
-        acc=sum(Y==Y_est)*1.0/len(Y)
-        return acc
+    Theta=np.array(list(map(lambda x:theta_(X,Beta[:,x],Gamma[:,:,x]),range(Beta.shape[1]))))  
+    sTheta=1.0/(1+np.exp(-Theta))
+    stheta=np.apply_along_axis(np.mean, 0, sTheta)
+    Y_est=np.array([0 if x<0.5 else 1 for x in stheta[0]])
+    Y_est=Y_est.reshape((len(Y_est),))
+    return sum(Y==Y_est)*1.0/len(Y)
 
-
-        
-
-def test_accuracy(theta_var,vr_beta_mean,vr_beta_rho,vr_gamma_mean,vr_gamma_rho):
+def test_accuracy(False,vr_beta_mean,vr_beta_rho,vr_gamma_mean,vr_gamma_rho):
     Beta,Gamma=q_simulate(10000,vr_beta_mean,vr_beta_rho,vr_gamma_mean,vr_gamma_rho)
-    beta=np.apply_along_axis(np.mean, 1, Beta)
-    gamma=np.apply_along_axis(np.mean, 2, Gamma)
-    n=X_test.shape[0]
-    X1=np.column_stack((np.ones(n),X_test))
-    V1=np.dot(gamma,X1.T)
-    V2=1.0/(1+np.exp(-V1))
-    theta=beta[0]+np.dot(beta[1::].reshape(1,kn),V2)
-    if theta_var==True:
-        var_theta=np.std(theta,axis=1)
-        Y_est=np.array([0 if x<0.5 else 1 for x in 1.0/(1+np.exp(-theta.T))])
-        Y_est=Y_est.reshape((len(Y_est),))
-        acc=sum(Y_test==Y_est)*1.0/len(Y_test)
-        return acc, var_theta
-    else:
-        Y_est=np.array([0 if x<0.5 else 1 for x in 1.0/(1+np.exp(-theta.T))])
-        Y_est=Y_est.reshape((len(Y_est),))
-        acc=sum(Y_test==Y_est)*1.0/len(Y_test)
-        return acc
+    Theta=np.array(list(map(lambda x:theta_(X_test,Beta[:,x],Gamma[:,:,x]),range(Beta.shape[1]))))  
+    sTheta=1.0/(1+np.exp(-Theta))
+    stheta=np.apply_along_axis(np.mean, 0, sTheta)
+    Y_est=np.array([0*1.0 if x<0.5 else 1 for x in stheta[0]])
+    return sum(Y_test==Y_est)*1.0/len(Y_test)
 
 
 def initialization(c,nodes):
